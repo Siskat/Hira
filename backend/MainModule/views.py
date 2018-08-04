f4152016b749caea80956d1d634922a986537eed
Zarhayda Tun <zarhaydatun@zarhaydas-mbp.ait-sydney.net.au>
2018-08-04 19:07:11 +1000

    front end stuff

--- backend/MainModule/views.py
+++ backend/MainModule/views.py
@@ -12,36 +12,32 @@ def page_not_found(e):
 @app.route("/")
 @app.route("/index")
 def index():
-        return render_template('index.html', sidebar=False)
+    return render_template('index.html', sidebar=False)
 
 @app.route("/login")
 def login():
-        return render_template('login.html', sidebar=False)
+    return render_template('login.html', sidebar=False)
 
-@app.route("/doctor_interface")
-def doctor_interface():
-        return render_template('doctor_interface.html', sidebar=False)
-
-@app.route("/nurse_interface")
-def  nurse_interface():
-        return render_template('nurse_interface.html', sidebar=False)
+@app.route("/appointments")
+def appointments():
+    return render_template('appointments.html', sidebar=False)
 
 @app.route("/patient")
 def patient():
-        return render_template('patient.html', sidebar=False)
+    return render_template('patient.html', sidebar=False)
 
 @app.route("/patients_list")
 def patients_list():
-        return render_template('patients_list.html', sidebar=False)
+    return render_template('patients_list.html', sidebar=False)
 
 @app.route("/media")
 def media():
-        return render_template('media.html', sidebar=False)
+    return render_template('media.html', sidebar=False)
 
 @app.route("/discharge")
 def discharge():
-        return render_template('discharge.html', sidebar=False)
+    return render_template('discharge.html', sidebar=False)
 
 @app.route("/patient_history")
 def patient_history():
-        return render_template('patient_history.html', sidebar=False)
+    return render_template('patient_history.html', sidebar=False)
