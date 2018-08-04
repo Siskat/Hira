7e35dfefc0999c0b3b72b4b9e01515ee25b9289f
= <alinaeem2006@gmail.com>
2018-08-04 16:32:33 +1000

    New database

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = True
 # database configuration settings
 DB_USERNAME = 'mpadmin'
 DB_PASSWORD = 'GulabJamun'
-DATABASE_NAME = 'meetup_point'
+DATABASE_NAME = 'anything_really'
 DB_HOST = os.getenv('IP','128.199.216.13')
 DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, DATABASE_NAME)
 SQLALCHEMY_DATABASE_URI = DB_URI
