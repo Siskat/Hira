e23f9562f507eec98f514bff00ae4966bb23f028
JamJamJames98 <thebug98@hotmail.com>
2018-08-04 16:15:46 +1000

    Fixes

--- backend/MainModule/views.py
+++ backend/MainModule/views.py
@@ -7,11 +7,25 @@ from backend import app
 
 @app.errorhandler(Exception)
 def page_not_found(e):
-    flash("Page not found")
     return render_template('index.html')
-
 
 @app.route("/")
 @app.route("/index")
 def index():
         return render_template('index.html', sidebar=False)
+
+@app.route("/login")
+def login():
+        return render_template('login.html', sidebar=False)
+
+@app.route("/doctor_interface")
+def doctor_interface():
+        return render_template('doctor_interface.html', sidebar=False)
+
+@app.route("/nurse_interface")
+def  nurse_interface():
+        return render_template('nurse_interface.html', sidebar=False)
+
+@app.route("/patient")
+def patient():
+        return render_template('patient.html', sidebar=False)
