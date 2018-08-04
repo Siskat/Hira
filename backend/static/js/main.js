33dd4c2bba41040303a88516d8a307bf20ef1082
Alvin Ho <alvinhoseaho@gmail.com>
2018-08-05 08:13:03 +1000

    Fixed modal popup

--- backend/static/js/main.js
+++ backend/static/js/main.js
@@ -36,10 +36,10 @@ $(function () {
     $("#count").text(counter);
   });
 
-  // $(document).ready(function(){
-  //     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-  //     $('.modal-trigger').leanModal();
-  // });
+  $(document).ready(function(){
+      // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+      $('.modal-trigger').leanModal();
+  });
 
 });
 
