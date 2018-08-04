53850f329a9c0fc68b309d34dc3db7bb3a036e34
Zarhayda Tun <zarhaydatun@zarhaydas-mbp.ait-sydney.net.au>
2018-08-05 01:36:17 +1000

    fix broken shit

--- backend/MainModule/views.py
+++ backend/MainModule/views.py
@@ -29,114 +29,107 @@ from google.cloud.speech import types
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html', sidebar=False)
+	return render_template('index.html', sidebar=False)
 
 @app.route("/login")
 def login():
-    return render_template('login.html', sidebar=False)
+	return render_template('login.html', sidebar=False)
 
 @app.route("/records")
 def records():
-    return render_template('records.html', sidebar=False)
+	return render_template('records.html', sidebar=False)
 
 @app.route("/appointments")
 def appointments():
-    return render_template('appointments.html', sidebar=False)
+	return render_template('appointments.html', sidebar=False)
 
-@app.route("/patient/<string:id>")
-def patient(id):
-    sqlQuery = "SELECT * from patient WHERE patient_id='" + id + "'"
-    print(sqlQuery)
-    patient = db.session.execute(sqlQuery)
-    print(patient)
-    for x in patient:
-    	sqlAppointments = "SELECT * from appointments WHERE patient_id='" + id + '"'
-    	appointment = db.session.execute(sqlAppointments)
-    	records = {}
-    	for y in appointment:
-    		sqlRecords = "SELECT * from record WHERE appointment_id='" + y.appointment_id + '"'
-    		records.append(db.session.execute(sqlRecords))
-    return render_template('patient.html', patient=x, appointment=appointment, record=records)
-
-@app.route("/patient")
-def patient():
-    return render_template('patient.html', sidebar=False)
+@app.route("/patient_access/<string:id>")
+def patient_access(id):
+	sqlQuery = "SELECT * from patient WHERE patient_id='" + id + "'"
+	patient = db.session.execute(sqlQuery)
+	for x in patient:
+		sqlAppointments = "SELECT * from appointment WHERE patient_id='" + id + "'"
+		appointment = db.session.execute(sqlAppointments)
+		for y in appointment:
+			sqlRecords = "SELECT * from record WHERE appointment_id='" + y.appointment_id + "'"
+			records = db.session.execute(sqlRecords)
+		return render_template('patient.html', patient=x, appointment=appointment, record=records)
 
 @app.route("/patients_list")
 def patients_list():
-    sqlQuery = "SELECT * from patient"
-    patientList = db.session.execute(sqlQuery)
-    print(patientList)
-    return render_template('patients_list.html', patientList=patientList)
+	sqlQuery = "SELECT * from patient"
+	patientList = db.session.execute(sqlQuery)
+	print(patientList)
+	return render_template('patients_list.html', patientList=patientList)
 
 @app.route("/media")
 def media():
-    return render_template('media.html', sidebar=False)
+	return render_template('media.html', sidebar=False)
 
 @app.route("/discharge")
 def discharge():
-    return render_template('discharge.html', sidebar=False)
+	return render_template('discharge.html', sidebar=False)
 
 @app.route("/patient_history")
 def patient_history():
-    return render_template('patient_history.html', sidebar=False)
+	return render_template('patient_history.html', sidebar=False)
 
 @app.route("/record_audio", methods=['POST'])
 def record_audio():
-    #Setting up audio specifications
-    sampling_frequency = 44100
-    duration = 5 #seconds
-    channels = 1 #mono audio
+	#Setting up audio specifications
+	sampling_frequency = 44100
+	duration = 5 #seconds
+	channels = 1 #mono audio
 
-    #Setting up defaults as our specifications
-    sd.default.samplerate = sampling_frequency
-    sd.default.channels = channels
+	#Setting up defaults as our specifications
+	sd.default.samplerate = sampling_frequency
+	sd.default.channels = channels
 
-    #prints all available devices for sound input and output
-    #> for current input device
-    #< for current output device
-    #print(sd.query_devices())
+	#prints all available devices for sound input and output
+	#> for current input device
+	#< for current output device
+	#print(sd.query_devices())
 
-    #records for the duration in seconds, and pauses the running of the file while doing so
-    #print("Start recording")
-    my_recording = sd.rec((duration * sampling_frequency), blocking=True)
-    #print("Finished recording")
+	#records for the duration in seconds, and pauses the running of the file while doing so
+	#print("Start recording")
+	my_recording = sd.rec((duration * sampling_frequency), blocking=True)
+	#print("Finished recording")
 
-    #plays back the recording, need to sleep(duration) in order to pause the file while listening
-    #not needed to pause but is nice to listen without file running
-    #sd.play(my_recording)
-    #sleep(duration)
+	#plays back the recording, need to sleep(duration) in order to pause the file while listening
+	#not needed to pause but is nice to listen without file running
+	#sd.play(my_recording)
+	#sleep(duration)
 
-    #need to save the numpy array (thats what my_recording is) as an audio file to be used for google
-    #speech to text API
-    file_name_wav = 'recordings/output.wav'
-    sf.write(file_name_wav, my_recording, sampling_frequency)
+	#need to save the numpy array (thats what my_recording is) as an audio file to be used for google
+	#speech to text API
+	file_name_wav = 'recordings/output.wav'
+	sf.write(file_name_wav, my_recording, sampling_frequency)
 
-    #google speech to text setup and processing
-    #Sets up credentials from API key
-    credentials = service_account.Credentials.from_service_account_file('recordings/494e73d46153.json')
+	#google speech to text setup and processing
+	#Sets up credentials from API key
+	credentials = service_account.Credentials.from_service_account_file('recordings/494e73d46153.json')
 
-    #Instantiates a client
-    client = speech.SpeechClient(credentials=credentials)
+	#Instantiates a client
+	client = speech.SpeechClient(credentials=credentials)
 
-    while (os.path.exists("recordings/output.wav") == False):
-        sleep(0.5)
+	while (os.path.exists("recordings/output.wav") == False):
+		sleep(0.5)
 
-    #Loads the audio into memory
-    with io.open(file_name_wav, 'rb') as audio_file:
-        content = audio_file.read()
-        audio = types.RecognitionAudio(content=content)
-    config = types.RecognitionConfig(encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,sample_rate_hertz=sampling_frequency,language_code='en-US')
+	#Loads the audio into memory
+	with io.open(file_name_wav, 'rb') as audio_file:
+		content = audio_file.read()
+		audio = types.RecognitionAudio(content=content)
+	config = types.RecognitionConfig(encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,sample_rate_hertz=sampling_frequency,language_code='en-US')
 
-    #Detects speech in the audio file
-    response = client.recognize(config, audio)
+	#Detects speech in the audio file
+	response = client.recognize(config, audio)
 
-    #Prints the
-    transcript = ""
-    for result in response.results:
-        transcript = transcript + result.alternatives[0].transcript
+	#Prints the
+	transcript = ""
+	for result in response.results:
+		transcript = transcript + result.alternatives[0].transcript
 
-    print("Transcript")
-    print(transcript)
+	print("Transcript")
+	print(transcript)
 
-    return render_template('index.html', sidebar=False);
+	return render_template('index.html', sidebar=False);
