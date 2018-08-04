3e9eac39734e5c669cb92f092a45539b1540f56e
HameshRanpara <gyradoshr@gmail.com>
2018-08-05 01:03:07 +1000

    backend records

--- backend/MainModule/views.py
+++ backend/MainModule/views.py
@@ -35,7 +35,13 @@ def patient(id):
     patient = db.session.execute(sqlQuery)
     print(patient)
     for x in patient:
-        return render_template('patient.html', patient=x)
+    	sqlAppointments = "SELECT * from appointments WHERE patient_id='" + id + '"'
+    	appointment = db.session.execute(sqlAppointments)
+    	records = {}
+    	for y in appointment:
+    		sqlRecords = "SELECT * from record WHERE appointment_id='" + y.appointment_id + '"'
+    		records.append(db.session.execute(sqlRecords))
+    return render_template('patient.html', patient=x, appointment=appointment, record=records)
 
 
 @app.route("/patients_list")
